10e180dd85a9256c9ce7c3d4fb0b232e8bf79c75
danand <danand1101@gmail.com>
2022-11-27 11:35:32 +0100

    gspread

--- factors.py
+++ factors.py
@@ -1,9 +1,41 @@
+from google.oauth2.service_account import Credentials
 import csv
 import streamlit as st
 import pandas as pd
 
 # Import DictWriter class from CSV module
 from csv import DictWriter
+
+from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
+
+from datetime import datetime
+import gspread
+
+scopes = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+path_cred = "C:/Users/DAnand/AppData/Roaming/gspread/credentials.json"
+
+credentials = Credentials.from_service_account_file(
+    path_cred,
+    scopes=scopes
+)
+
+gc = gspread.authorize(credentials)
+sheet = gc.open("stud_data").sheet1
+
+# function to get current time
+
+
+def date_now():
+    now = datetime.now()
+    mydate = datetime.strftime(now, '%Y-%m-%d %H:%M:%S')
+    return mydate
 
 
 if __name__ == "__main__":
@@ -14,8 +46,8 @@ if __name__ == "__main__":
         "Kindly select Yes/No for the factors that you think are important in getting the first job. ")
 
     stream = st.selectbox(
-        "Does the studnet's stream of study have a direct relation to the job?", ["No", "Yes"])
-    uni = st.selectbox("Dose the university matters?", ["No", "Yes"])
+        "Does the student's stream of study have a direct relation to the job?", ["No", "Yes"])
+    uni = st.selectbox("Dose the university matter?", ["No", "Yes"])
     grade = st.selectbox("Does the student's grades matter?", ["No", "Yes"])
     old_job = st.selectbox(
         "Dose having a job in India before your first job in Germany matter?", ["No", "Yes"])
@@ -33,6 +65,7 @@ if __name__ == "__main__":
     network = st.selectbox(
         "Does the student's network matter?", ["No", "Yes"])
     other = st.text_input('Anything else you think could be important', None)
+    date = date_now()
 
     input_dict = {
         "stream": stream,
@@ -46,11 +79,25 @@ if __name__ == "__main__":
         "projects": projects,
         "language": language,
         "network": network,
-        "other": other
+        "other": other,
+        "date": date
     }
 
-    field_names = ["stream", "uni", "grade", "old_job", "internship",
-                   "num_experience", "age", "thesis", "projects", "language", "network", "other"]
+    field_names = [
+        "stream",
+        "uni",
+        "grade",
+        "old_job",
+        "internship",
+        "num_experience",
+        "age",
+        "thesis",
+        "projects",
+        "language",
+        "network",
+        "other",
+        "date"
+    ]
     if st.button('Save the information'):
         with open('stud_data.csv', 'a', newline='') as f_object:
             # Pass the file object and a list
@@ -59,4 +106,6 @@ if __name__ == "__main__":
             dictwriter_object = DictWriter(f_object, fieldnames=field_names)
             # Pass the dictionary as an argument to the Writerow()
             dictwriter_object.writerow(input_dict)
+            # collect = [height,sex_no,shoe_size,date_time]
+            sheet.insert_row(input_dict, 2)
         st.success("Data saved successfully")
